f10138aeb3fc01f29c7b6d69e97266715fea6679
Nicholas Farrell <O1as_mail@yahoo.com>
2019-12-13 15:47:08 -0600

    Add files via upload

---
+++ cobascripts.js
@@ -0,0 +1,5 @@
+function insertwidget(name, pic, color, title, item1="item1", item2="item2"){
+        document.getElementById(name).innerHTML = "<div style='display: inline-block;'><div style='float:left;width:750px;'><img alt='abh-header.jpg' style='width: 100%;' src='" + pic + "' /></div><div style='position: relative;color: #ffffff;float:left;  width:250px;height: 437px;background-color: " + color +"'><div style='position: absolute; top: 31%; left: -20%; font-size: 700%; opacity: 0.3; transform: rotate(90deg);'>" + title + "</div><div style='position: absolute; bottom: 20px; left: 20px;'>" + item1 + "<br />" + item2 + "<br /> Seminar Rooms <br /> Group Learning Environments <br /> A Trading Room <br /> Community Space <br /> Advising Centers <br /> Faculty Offices</div></div></div>";
+        
+        
+    }
